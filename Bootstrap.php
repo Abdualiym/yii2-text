7bb050a890095f36b713aea43d945f190bd026c3
abdualiym <a.abdualiym@gmail.com>
2018-03-23 19:49:22 +0500

    bootstrap finish

--- Bootstrap.php
+++ Bootstrap.php
@@ -14,21 +14,11 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
-        // Add module URL rules.
-//        Yii::$app->urlManager->addRules(
-//            [
-//                'captcha'=>'/site/captcha',
-//                'feedback' => 'contacts/index',
-//            ],
-//            false
-//        );
-
         // Add module I18N category.
         if (!isset($app->i18n->translations['text']) && !isset($app->i18n->translations['text*'])) {
             $app->i18n->translations['text'] = [
                 'class' => 'yii\i18n\PhpMessageSource',
-                'basePath' => '@abdualiym/contacts/messages',
-//                'basePath' => '@vendor/abdualiym/yii2-text/messages',
+                'basePath' => '@abdualiym/text/messages',
                 'sourceLanguage' => 'en',
                 'forceTranslation' => true,
             ];
